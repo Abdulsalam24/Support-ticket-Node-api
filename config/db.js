20a14cb18b7c3e0da10331151bcd7a1d1fb20e60
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2022-10-22 22:04:27 +0100

    backend commit

--- config/db.js
+++ config/db.js
@@ -11,5 +11,4 @@ const connectDb = async () => {
 }
 
 
-
 module.exports = connectDb
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express")
+const path = require("path")
+
 const colors = require("colors")
 const { errorHandler } = require("./middleware/errorMiddleware")
 const connectDb = require("./config/db")
@@ -16,14 +18,21 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-//landing page
-app.get('/', (req, res) => {
-    res.status(200).json({ message: "Welcome to the landing" })
-})
-
 //routes middleware
 app.use("/api/users", require("./routes/userRoutes"))
 app.use("/api/tickets", require("./routes/ticketRoutes"))
+
+//server frontend
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")))
+
+    app.get("*", (req, res) => res.sendFile(__dirname, "../", "frontend", "build", "index.html"))
+} else {
+    //landing page
+    app.get('/', (req, res) => {
+        res.status(200).json({ message: "Welcome to the landing" })
+    })
+}
 
 //error middleware
 app.use(errorHandler)
