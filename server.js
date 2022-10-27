a7ee3ea9c3b0a83bbb0d69e1e5dfaafd308dd66b
Abdulsalam Mohammed <abdulsalammohammed586686@gmail.com>
2022-10-27 11:19:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -23,16 +23,16 @@ app.use("/api/users", require("./routes/userRoutes"))
 app.use("/api/tickets", require("./routes/ticketRoutes"))
 
 //server frontend
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/build")))
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "../frontend/build")))
 
-    app.get("*", (req, res) => res.sendFile(__dirname, "../", "frontend", "build", "index.html"))
-} else {
+//     app.get("*", (req, res) => res.sendFile(__dirname, "../", "frontend", "build", "index.html"))
+// } else {
     //landing page
     app.get('/', (req, res) => {
         res.status(200).json({ message: "Welcome to the landing" })
     })
-}
+// }
 
 //error middleware
 app.use(errorHandler)
